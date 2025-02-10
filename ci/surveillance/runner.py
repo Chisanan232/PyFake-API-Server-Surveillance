46afdb214f8f6e8cc81899fc5b67549be45536f4
Chisanan232 <chi10211201@cycu.org.tw>
2025-02-10 17:01:17 +0800

    ➕ Implement the logic about getting the parameter values from GitHub action inputs.

--- ci/surveillance/runner.py
+++ ci/surveillance/runner.py
@@ -1,8 +1,14 @@
+import os
+
+from .model.action import ActionInput
+
+
 def run() -> None:
     # get the API doc config from end point (request API doc config and get response)
     # check the diff between local config and the new config (check the diff by git?)
     # if no diff = nothing, else it would update the config (commit the change and request PR by git and gh?)
     print("monitor the github repro ...")
+    action_inputs = ActionInput.deserialize(os.environ)
     # result = Surveillance.monitor()
     print("got difference~")
     # GitHelper.commit_change()
