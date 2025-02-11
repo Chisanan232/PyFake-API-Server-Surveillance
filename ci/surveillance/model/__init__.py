2f68380b4372324e285a7927f18e69506a63b2b6
Chisanan232 <chi10211201@cycu.org.tw>
2025-02-11 12:13:19 +0800

    ✏️ Add section info to comment the target.

--- ci/surveillance/model/__init__.py
+++ ci/surveillance/model/__init__.py
@@ -2,9 +2,11 @@ from enum import Enum
 
 
 class EnvironmentVariableKey(Enum):
-    GIT_REPOSITORY = "GIT_REPOSITORY"
+    # API documentation info
     API_DOC_URL = "API_DOC_URL"
     SERVER_TYPE = "SERVER_TYPE"
+    # git info
+    GIT_REPOSITORY = "GIT_REPOSITORY"
     GIT_AUTHOR_NAME = "GIT_AUTHOR_NAME"
     GIT_AUTHOR_EMAIL = "GIT_AUTHOR_EMAIL"
     GIT_COMMIT_MSG = "GIT_COMMIT_MSG"
