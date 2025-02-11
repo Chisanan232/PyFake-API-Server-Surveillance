5a384eb0e4fd42549a7fe012504546e705b85da3
Chisanan232 <chi10211201@cycu.org.tw>
2025-02-11 12:59:14 +0800

    ➕ Implement downloading the PyFake-API-Server configuration from API document config.

--- test/unit_test/surveillance/runner.py
+++ test/unit_test/surveillance/runner.py
@@ -21,7 +21,12 @@ class DummySwaggerAPIDocConfigResponse(BaseHTTPResponse):
 
     @property
     def data(self) -> bytes:
-        _data = {"swagger": "v2.0.1"}
+        _data = {
+            "swagger": "2.0",
+            "tags": [],
+            "paths": {},
+            "definitions": {},
+        }
         return json.dumps(_data).encode("utf-8")
 
 
@@ -29,12 +34,24 @@ class DummySwaggerAPIDocConfigResponse(BaseHTTPResponse):
 @patch("urllib3.request")
 def test_run(mock_request: Mock, api_doc_config_resp: Type[BaseHTTPResponse]):
     data = {
+        # API documentation info
         EnvironmentVariableKey.API_DOC_URL.value: "http://10.20.0.13:8080",
         EnvironmentVariableKey.SERVER_TYPE.value: "rest-server",
+        # git info
         EnvironmentVariableKey.GIT_REPOSITORY.value: "test/sample-project",
         EnvironmentVariableKey.GIT_AUTHOR_NAME.value: "test",
         EnvironmentVariableKey.GIT_AUTHOR_EMAIL.value: "test@gmail.com",
         EnvironmentVariableKey.GIT_COMMIT_MSG.value: "✏️ Update the API interface settings.",
+        # for subcommand line *pull* options
+        EnvironmentVariableKey.CONFIG_PATH.value: "./api.yaml",
+        EnvironmentVariableKey.INCLUDE_TEMPLATE_CONFIG.value: "True",
+        EnvironmentVariableKey.BASE_FILE_PATH.value: "./",
+        EnvironmentVariableKey.BASE_URL.value: "/test/v1",
+        EnvironmentVariableKey.DIVIDE_API.value: "true",
+        EnvironmentVariableKey.DIVIDE_HTTP.value: "false",
+        EnvironmentVariableKey.DIVIDE_HTTP_REQUEST.value: "false",
+        EnvironmentVariableKey.DIVIDE_HTTP_RESPONSE.value: "false",
+        EnvironmentVariableKey.DRY_RUN.value: "true",
     }
     mock_request.return_value = api_doc_config_resp(
         request_url=data[EnvironmentVariableKey.API_DOC_URL.value],
