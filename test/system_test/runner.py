bb0bd8191aed976d09d9cb52ce340d8a7ee2a218
Chisanan232 <chi10211201@cycu.org.tw>
2025-02-18 12:55:33 +0800

    👨‍💻 Add log message for troubleshooting of fail test in PR.

--- test/system_test/runner.py
+++ test/system_test/runner.py
@@ -117,6 +117,12 @@ def test_entire_flow_with_not_exist_config(
             print(f"[DEBUG] commit.author: {c.author}")
             print(f"[DEBUG] commit.message: {c.message}")
 
+        head_commit = repo.head.commit
+        print(f"[DEBUG] head_commit: {head_commit}")
+        print(f"[DEBUG] head_commit.hexsha: {head_commit.hexsha}")
+        print(f"[DEBUG] head_commit.author: {head_commit.author}")
+        print(f"[DEBUG] head_commit.message: {head_commit.message}")
+
         assert repo.head.commit.author.name == data[EnvironmentVariableKey.GIT_AUTHOR_NAME.value]
         assert repo.head.commit.author.email == data[EnvironmentVariableKey.GIT_AUTHOR_EMAIL.value]
         assert repo.head.commit.message == data[EnvironmentVariableKey.GIT_COMMIT_MSG.value]
