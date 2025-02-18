620c798750055af2562bc667f6165c7622e7bc55
Chisanan232 <chi10211201@cycu.org.tw>
2025-02-18 13:11:47 +0800

    ✏️ Fix the fail test at correct place.

--- test/integration_test/surveillance/runner.py
+++ test/integration_test/surveillance/runner.py
@@ -130,6 +130,11 @@ def test_commit_change_config(mock_init_remote_fun: Mock, mock_git_commit: Mock)
         if not os.getenv("GITHUB_ACTIONS") and str(filepath) in committed_files:
             # test finally
             real_repo.git.restore("--staged", str(filepath))
-        if real_repo.active_branch != original_branch:
+        try:
+            current_branch = real_repo.active_branch
+        except:
+            print("[DEBUG] Occur something wrong when trying to get git branch")
+            current_branch = "DETACHED_" + real_repo.head.object.hexsha
+        if current_branch != original_branch:
             real_repo.git.checkout(original_branch)
             real_repo.git.branch("-D", git_branch_name)
