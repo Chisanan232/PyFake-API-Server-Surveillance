411e237f0e0e0e062a41ec3e963958e5673d63ac
Chisanan232 <chi10211201@cycu.org.tw>
2025-02-19 12:35:31 +0800

    ✏️ Setting the git repository.

--- ci/surveillance/runner.py
+++ ci/surveillance/runner.py
@@ -38,7 +38,7 @@ def commit_change_config(action_inputs: ActionInput) -> bool:
         github_access_token = os.environ["FAKE_API_SERVER_BOT_GITHUB_TOKEN"]
         git_ssh_access = f"{github_account}:{github_access_token}@"
         git_remote.create(
-            name=remote_name, url=f"https://{git_ssh_access}github.com/{action_inputs.git_info.repository}"
+            repo=repo, name=remote_name, url=f"https://{git_ssh_access}github.com/{action_inputs.git_info.repository}"
         )
 
     # Sync up the code version from git
