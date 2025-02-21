9521e28b80ce0a6da5c9c285f39751105722c01d
Chisanan232 <chi10211201@cycu.org.tw>
2025-02-21 15:08:21 +0800

    ♻️ Divide the action input arguments as some different parts.

--- test/_values/_test_data.py
+++ test/_values/_test_data.py
@@ -5,10 +5,20 @@ from ci.surveillance.model import EnvironmentVariableKey
 
 
 
-class fake_github_action_env:
+class fake_github_action_values:
     @classmethod
     def action_run_id(cls) -> str:
         return "123456"
+
+    @classmethod
+    def ci_env(cls, repo: str) -> Dict[str, str]:
+        return {
+            # GitHub action environment
+            "GITHUB_TOKEN": "ghp_1234567890",
+            "GITHUB_REPOSITORY": repo,
+            "GITHUB_HEAD_REF": "git-branch",
+            "GITHUB_RUN_ID": fake_github_action_values.action_run_id(),
+        }
 
 
 class fake_git_data:
@@ -18,7 +28,7 @@ class fake_git_data:
 
     @classmethod
     def fake_api_server_monitor_branch(cls) -> str:
-        return f"fake-api-server-monitor-update-config_{fake_github_action_env.action_run_id()}"
+        return f"fake-api-server-monitor-update-config_{fake_github_action_values.action_run_id()}"
 
 
 class fake_data:
@@ -28,15 +38,35 @@ class fake_data:
 
     @classmethod
     def action_input(cls, file_path: Union[str, Path], base_test_dir: Union[str, Path]) -> Dict[str, str]:
+        action_inputs = {}
+        action_inputs.update(cls.backend_project_info())
+        action_inputs.update(cls.git_operation_info())
+        action_inputs.update(cls.subcmd_pull_args(file_path=file_path, base_test_dir=base_test_dir))
+        action_inputs.update(cls.action_operation())
+        action_inputs.update(fake_github_action_values.ci_env(cls.repo()))
+        return action_inputs
+
+    @classmethod
+    def backend_project_info(cls) -> Dict[str, str]:
         return {
             # API documentation info
             EnvironmentVariableKey.API_DOC_URL.value: "http://127.0.0.1:8080",
             EnvironmentVariableKey.SERVER_TYPE.value: "rest-server",
+        }
+
+    @classmethod
+    def git_operation_info(cls) -> Dict[str, str]:
+        return {
             # git info
             EnvironmentVariableKey.GIT_REPOSITORY.value: cls.repo(),
             EnvironmentVariableKey.GIT_AUTHOR_NAME.value: "test-user[bot]",
             EnvironmentVariableKey.GIT_AUTHOR_EMAIL.value: "test-bot@localhost.com",
             EnvironmentVariableKey.GIT_COMMIT_MSG.value: " 🧪 test commit message",
+        }
+
+    @classmethod
+    def subcmd_pull_args(cls, file_path: Union[str, Path], base_test_dir: Union[str, Path]) -> Dict[str, str]:
+        return {
             # for subcommand line *pull* options
             EnvironmentVariableKey.CONFIG_PATH.value: str(file_path),
             EnvironmentVariableKey.INCLUDE_TEMPLATE_CONFIG.value: "True",
@@ -47,11 +77,11 @@ class fake_data:
             EnvironmentVariableKey.DIVIDE_HTTP_REQUEST.value: "false",
             EnvironmentVariableKey.DIVIDE_HTTP_RESPONSE.value: "false",
             EnvironmentVariableKey.DRY_RUN.value: "true",
+        }
+
+    @classmethod
+    def action_operation(cls) -> Dict[str, str]:
+        return {
             # operation with action in CI
             EnvironmentVariableKey.ACCEPT_CONFIG_NOT_EXIST.value: "false",
-            # GitHub action environment
-            "GITHUB_TOKEN": "ghp_1234567890",
-            "GITHUB_REPOSITORY": cls.repo(),
-            "GITHUB_HEAD_REF": "git-branch",
-            "GITHUB_RUN_ID": fake_github_action_env.action_run_id(),
         }
