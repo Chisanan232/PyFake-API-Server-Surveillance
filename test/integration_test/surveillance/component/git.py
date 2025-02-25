0dfc8548f3636d2e2808de9e59a19bc5a8c9b840
Chisanan232 <chi10211201@cycu.org.tw>
2025-02-25 22:53:19 +0800

    ♻️ Merge the integration tests about initialing git remote function.

--- test/integration_test/surveillance/component/git.py
+++ test/integration_test/surveillance/component/git.py
@@ -53,7 +53,8 @@ class TestGitOperation:
             if test_remote_name in [b.name for b in git_operation.repository.branches]:
                 git_operation.repository.git.branch("-D", test_remote_name)
 
-    def test_init_git_remote_with_not_exist_remote(self, git_operation: GitOperation):
+    @pytest.mark.parametrize("remote_already_exist", [False, True])
+    def test_init_git_remote(self, git_operation: GitOperation, remote_already_exist: bool):
         # given
         action_inputs = self._given_action_inputs
         test_remote_name = "pytest-origin"
@@ -61,34 +62,8 @@ class TestGitOperation:
         try:
             with patch("os.path.exists", return_value=True):
                 git_operation.repository = git_operation._init_git(self._given_action_inputs)
-
-            # when
-            dummy_ci_env = fake_github_action_values.ci_env(fake_data.repo())
-            with patch.dict(os.environ, dummy_ci_env, clear=True):
-                git_operation._init_git_remote(action_inputs=action_inputs, remote_name=test_remote_name)
-
-            # should
-            assert test_remote_name in git_operation.repository.remotes
-            remote = git_operation.repository.remote(name=test_remote_name)
-            assert remote.name == test_remote_name
-            assert (
-                remote.url
-                == f"https://x-access-token:{dummy_ci_env['GITHUB_TOKEN']}@github.com/{action_inputs.git_info.repository}"
-            )
-        finally:
-            # clean test
-            if test_remote_name in git_operation.repository.remotes:
-                git_operation.repository.delete_remote(remote=git_operation.repository.remote(name=test_remote_name))
-
-    def test_init_git_remote_with_exist_remote(self, git_operation: GitOperation):
-        # given
-        action_inputs = self._given_action_inputs
-        test_remote_name = "pytest-origin"
-
-        try:
-            with patch("os.path.exists", return_value=True):
-                git_operation.repository = git_operation._init_git(self._given_action_inputs)
-            git_operation.repository.create_remote(name=test_remote_name, url="https://test.com")
+            if remote_already_exist:
+                git_operation.repository.create_remote(name=test_remote_name, url="https://test.com")
 
             # when
             dummy_ci_env = fake_github_action_values.ci_env(fake_data.repo())
