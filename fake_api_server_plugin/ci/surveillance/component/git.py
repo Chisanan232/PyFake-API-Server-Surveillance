3bc2b2f53990bef3ae06acb304002fd59f70c5c5
Chisanan232 <chi10211201@cycu.org.tw>
2025-02-26 10:38:28 +0800

    ✏️ Fix broken code because refactor.

--- fake_api_server_plugin/ci/surveillance/component/git.py
+++ fake_api_server_plugin/ci/surveillance/component/git.py
@@ -109,7 +109,7 @@ class GitOperation:
         assert os.path.exists(
             action_inputs.subcmd_pull_args.config_path
         ), "PyFake-API-Server configuration is required. Please check it."
-        return Repo("/")
+        return Repo("./")
 
     def _init_git_remote(self, action_inputs: ActionInput, remote_name: str) -> Remote:
         if remote_name not in self.repository.remotes:
