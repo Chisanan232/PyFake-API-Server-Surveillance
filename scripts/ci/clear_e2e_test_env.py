79e6b807a9de87c1eefc0b06b894da218c9fc144
Chisanan232 <chi10211201@cycu.org.tw>
2025-03-07 22:42:03 +0800

    ✏️ Fix the bug at getting PR in cleaning process.

--- scripts/ci/clear_e2e_test_env.py
+++ scripts/ci/clear_e2e_test_env.py
@@ -65,7 +65,7 @@ def search_github_repo_pr(head_branch: str) -> PullRequest:
     one_page = prs.get_page(0)
     assert one_page
     print(f"[DEBUG] one_page {one_page}.")
-    target_pr = list(filter(lambda p: p.head == head_branch, one_page))
+    target_pr = list(filter(lambda p: p.head.ref == head_branch, one_page))
     print(f"[DEBUG] target_pr {target_pr}.")
     assert target_pr
     return one_page[0]
